f790d660db3bbf1e46823c7531175d865b529f91
jonLarriba333 <jonlarribaurrutia@gmail.com>
2026-03-06 09:20:03 +0100

    colouring the world script Find by tag
    A logic were aka and ao is differentiated has been added.

--- TestDia1/Assets/Scripts/Variables/ColourTheWorld.cs
+++ TestDia1/Assets/Scripts/Variables/ColourTheWorld.cs
@@ -9,49 +9,43 @@ public class ColourTheWorld : MonoBehaviour
     [SerializeField]
     private UnityEngine.Object[] allTheGameObjectsList;
 
+    [SerializeField]
+    private UnityEngine.Object[] tagItems;
+
     public bool findBlue;
     public bool findRed; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        if (findBlue==true && findRed == false)
-        {
-            allTheGameObjectsList = GameObject.FindGameObjectsWithTag("Ao");
-        }
-        else if (findRed==true && findBlue == false)
-        {
-            allTheGameObjectsList = GameObject.FindGameObjectsWithTag("Aka");
-        }
-        else if (findRed == false && findBlue == false)
-        {
-            Debug.Log("Nothing");
-        }
-        else
-        {
-            allTheGameObjectsList = FindObjectsByType<MeshRenderer>(FindObjectsSortMode.None);
-        }
-
-
+    { 
+       allTheGameObjectsList = FindObjectsByType<MeshRenderer>(FindObjectsSortMode.None); //This finds every object that has a mesh renderer inside.
     }
 
     // Update is called once per frame
     void Update()
     {
-      //  for (int i = 0; i < allTheGameObjectsList.Length; i++)
-       // {
-       //     if (allTheGameObjectsList[i].GetComponent<MeshRenderer>())
-         //   {
-           //     if (GameObject (allTheGameObjectsList[i].) == "Aka")
-             //   {
-            //        allTheGameObjectsList[i].GetComponent<MeshRenderer>().material.color = Color.red;
-             //   }
-           //     else if (allTheGameObjectsList[i].GetComponent<GameObject>().tag == "Ao")
-             //   {
-               //     allTheGameObjectsList[i].GetComponent<MeshRenderer>().material.color = Color.blue;
-           //     }
-             //  // allTheGameObjectsList[i].GetComponent<MeshRenderer>().material.color = colour;
-            //}
-        //}
+        if (findBlue == true && findRed == false)
+        {
+            tagItems = GameObject.FindGameObjectsWithTag("Ao"); //Includes in the list every single object with blue tag
+        }
+        else if (findRed == true && findBlue == false)
+        {
+            tagItems = GameObject.FindGameObjectsWithTag("Aka"); // Includes in the list every single object with red tag
+        }
+        else if (findRed == false && findBlue == false)
+        {
+            tagItems = null;
+        }
+        else
+        {
+            tagItems = FindObjectsByType<MeshRenderer>(FindObjectsSortMode.None); //This finds every object that has a mesh renderer inside.
+
+        }
+
+
+        for (int i = 0; i < allTheGameObjectsList.Length; i++)
+            {
+                tagItems[i].GetComponent<MeshRenderer>().material.color = colour; //This paints every single GameObject
+            }
     }
 }
