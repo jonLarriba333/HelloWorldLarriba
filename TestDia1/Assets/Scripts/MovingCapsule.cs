dfe4af03dce8d4387a2d6346e74f874a79f2593c
Jon Larriba <257492421+jonLarriba333@users.noreply.github.com>
2026-03-12 09:35:59 +0100

    Refactor of move method
    also added less debugs

--- TestDia1/Assets/Scripts/MovingCapsule.cs
+++ TestDia1/Assets/Scripts/MovingCapsule.cs
@@ -12,39 +12,37 @@ public class MovingCapsule : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CapsuleMover();
+        CapsuleMove();
     }
 
-    void CapsuleMover()
+    void CapsuleMove()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             gameObject.transform.position += Vector3.forward  * velocity * Time.deltaTime;
-            Debug.Log(gameObject.transform.position);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            gameObject.transform.position += Vector3.left * velocity * Time.deltaTime;
-            Debug.Log(gameObject.transform.position);
+            gameObject.transform.position += Vector3.left * velocity * Time.deltaTime;   
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             gameObject.transform.position += Vector3.back * velocity * Time.deltaTime;
-            Debug.Log(gameObject.transform.position);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             gameObject.transform.position += Vector3.right * velocity * Time.deltaTime;
-            Debug.Log(gameObject.transform.position);
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             gameObject.transform.position += Vector3.up * velocity * Time.deltaTime;
-            Debug.Log(gameObject.transform.position);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             gameObject.transform.position += Vector3.down * velocity * Time.deltaTime;
+        }
+        if (Input.anyKeyDown)
+        {
             Debug.Log(gameObject.transform.position);
         }
     }
